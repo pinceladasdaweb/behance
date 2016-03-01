e27444801ab5735ed20a817ac3fe2c9157d86da1
Pedro Rogério <pinceladasdaweb@hotmail.com>
2016-03-01 12:56:37 -0300

    Plugin refactored

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,86 @@
-/*
---------------------------------
-Behance Portfolio Page
---------------------------------
-+ https://github.com/pinceladasdaweb/Behance-Portfolio-Page
-+ version 1.0.0
-+ Copyright 2014 Pedro Rogerio
-+ Licensed under the MIT license
-
-+ Documentation: https://github.com/pinceladasdaweb/Behance-Portfolio-Page
-*/
-var Be = (function (window, document, undefined) {
+/*jslint browser: true, debug: true*/
+/*global define, module, exports*/
+(function (root, factory) {
     "use strict";
-    var module = {
-        config: function (config) {
-            this.url    = './request.php?user=' + config.user;
-            this.body   = document.querySelector('body');
-            this.loader = document.querySelector('.loading'); 
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.Behance = factory();
+    }
+}(this, function () {
+    "use strict";
+
+    if (!(Function.prototype.hasOwnProperty('bind'))) {
+        Function.prototype.bind = function () {
+            var fn = this, context = arguments[0], args = Array.prototype.slice.call(arguments, 1);
+            return function () {
+                return fn.apply(context, args.concat(Array.prototype.slice.call(arguments)));
+            };
+        };
+    }
+
+    var Behance = function (options) {
+        if (!this || !(this instanceof Behance)) {
+            return new Behance(options);
+        }
+
+        if (!options) {
+            options = {};
+        }
+
+        if (!options.user) {
+            throw 'Provide a valid Behance user name';
+        }
+
+        this.endpoint = './request.php?user=' + options.user;
+        this.body     = document.querySelector('body');
+        this.loader   = document.querySelector('.loading'); 
+
+        this.request();
+    };
+
+    Behance.prototype = {
+        getJSON: function (path, success, fail) {
+            var xhttp = new XMLHttpRequest();
+
+            xhttp.open('GET', path, true);
+            xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+            xhttp.setRequestHeader('Content-type', 'application/json');
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if ((this.status >= 200 && this.status < 300) || this.status === 304) {
+                        var response = JSON.parse(this.responseText);
+
+                        success.call(this, response);
+                    } else {
+                        fail.call(this, this.status + ' - ' + this.statusText);
+                    }
+                }
+            };
+            xhttp.send();
+            xhttp = null;
         },
         supportsSvg: function() {
             return document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
         },
-        xhr: function () {
-            return new window.XMLHttpRequest();
-        },
-        getJSON: function (options, callback) {
-            var xhttp = module.xhr();
-            options.url = options.url || location.href;
-            options.data = options.data || null;
-            xhttp.open('GET', options.url, true);
-            xhttp.send(options.data);
-            xhttp.onreadystatechange = function () {
-                if (xhttp.status === 200 && xhttp.readyState === 4) {
-                    callback(JSON.parse(xhttp.responseText));
-                }
-            };
-        },
-        create: function (name, props) {
+        createEls: function (name, props, text) {
             var el = document.createElement(name), p;
+
             for (p in props) {
                 if (props.hasOwnProperty(p)) {
                     el[p] = props[p];
                 }
             }
+
+            if (text) {
+                el.appendChild(document.createTextNode(text));
+            }
+
             return el;
         },
-        loop: function (els, callback) {
+        each: function (els, callback) {
             var i = 0, max = els.length;
 
             while (i < max) {
@@ -52,186 +88,147 @@ var Be = (function (window, document, undefined) {
                 i += 1;
             }
         },
-        userProfile: function (data) {
-            var self         = this,
-                header       = document.querySelector('#header'),
-                displayName  = document.createTextNode(data.user.display_name),
-                avatar       = data.user.images[138],
-                fields       = data.user.fields,
-                localization = document.createTextNode(data.user.location),
-                elements     = document.createDocumentFragment(),
+        structureHeader: function () {
+            var elements = document.createDocumentFragment(), header;
+
+            header = this.createEls('header', { className: 'portfolio-header clearfix', id: 'header' });
+
+            elements.appendChild(header);
+            this.body.appendChild(elements);
+
+            return this;
+        },
+        structureContent: function () {
+            var elements = document.createDocumentFragment(),
+                content, portfolio, portfolioList;
+
+            content       = this.createEls('div', { className: 'content-area clearfix', id: 'content' });
+            portfolio     = this.createEls('div', { className: 'portfolio-area clearfix', id: 'portfolio' });
+            portfolioList = this.createEls('ul', { className: 'portfolio-list clearfix' });
+
+            portfolio.appendChild(portfolioList);
+            content.appendChild(portfolio);
+
+            elements.appendChild(content);
+            this.body.appendChild(elements);
+
+            return this;
+        },
+        structureFooter: function () {
+            var elements = document.createDocumentFragment(),
+                footer, power, powered, p, a;
+
+            footer  = this.createEls('footer', { className: 'portfolio-footer clearfix', id: 'footer' });
+            power   = this.createEls('div', { className: 'power-by', id: 'power' });
+            powered = this.createEls('p', {}, 'Powered by');
+            a       = this.createEls('a', { className: 'power-logo fi-social-behance', href: 'http://www.behance.net/', title: 'Behance' }, 'Behance');
+            p       = this.createEls('p');
+
+            p.appendChild(a);
+            power.appendChild(powered);
+            power.appendChild(p);
+            footer.appendChild(power);
+
+            elements.appendChild(footer);
+            this.body.appendChild(elements);
+
+            return this;
+        },
+        mountProfile: function (data) {
+            var header   = document.querySelector('#header'),
+                fields   = data.user.fields,
+                elements = document.createDocumentFragment(),
                 fieldName, figure, img, h1, ul, li, div, divF;
 
-            figure = self.create('figure', { className: 'profile-avatar'});
-            img    = self.create('img', { src: avatar });
-            h1     = self.create('h1', { className: 'profile-name' });
-            ul     = self.create('ul', { className: 'field-list' });
-            divF   = self.create('div', { className: 'profile-fields' });
-            div    = self.create('div', { className: 'profile-location fi-marker' });
+            figure = this.createEls('figure', { className: 'profile-avatar'});
+            img    = this.createEls('img', { src: data.user.images[138] });
+            h1     = this.createEls('h1', { className: 'profile-name' }, data.user.display_name);
+            ul     = this.createEls('ul', { className: 'field-list' });
+            divF   = this.createEls('div', { className: 'profile-fields' });
+            div    = this.createEls('div', { className: 'profile-location fi-marker' }, data.user.location);
 
-            self.loop(fields, function (field) {
-                fieldName = document.createTextNode(field);
-                li = self.create('li', { className: 'field-item' });
+            this.each(fields, function (field) {
+                li = this.createEls('li', { className: 'field-item' }, field);
 
-                li.appendChild(fieldName);
                 ul.appendChild(li.cloneNode(true));
-            });
+            }.bind(this));
 
             figure.appendChild(img);
-            h1.appendChild(displayName);
             divF.appendChild(ul);
-            div.appendChild(localization);
 
             elements.appendChild(figure);
             elements.appendChild(h1);
             elements.appendChild(divF);
             elements.appendChild(div);
             header.appendChild(elements);
+
+            return this;
         },
-        userProjects: function (data) {
-            var self   = this;
+        mountProjects: function (data) {
+            var elements = document.createDocumentFragment(),
+                fields, portfolio, h2, ul, li, liF, a, div, divF, figure, img, i, len;
 
-            self.loop(data.projects, function (project) {
-                var url          = project.url,
-                    cover        = project.covers[404],
-                    projectName  = document.createTextNode(project.name),
-                    projectTitle = project.name,
-                    fields       = project.fields,
-                    portfolio    = document.querySelector('.portfolio-list'),
-                    elements     = document.createDocumentFragment(),
-                    fieldName, h2, ul, li, liF, a, div, divF, figure, img, i, len;
+            this.each(data.projects, function (project) {
+                fields       = project.fields;
+                portfolio    = document.querySelector('.portfolio-list');
 
-                ul     = self.create('ul', { className: 'field-list' });
-                li     = self.create('li', { className: 'portfolio-item' });
-                div    = self.create('div', { className: 'portfolio-content' });
-                divF   = self.create('div', { className: 'portfolio-fields' });
-                figure = self.create('figure', { className: 'portfolio-cover', title: projectTitle });
-                a      = self.create('a', { href: url });
-                img    = self.create('img', { className: 'portfolio-image', src: cover });
-                h2     = self.create('h2', { className: 'portfolio-title' });
+                ul     = this.createEls('ul', { className: 'field-list' });
+                li     = this.createEls('li', { className: 'portfolio-item' });
+                div    = this.createEls('div', { className: 'portfolio-content' });
+                divF   = this.createEls('div', { className: 'portfolio-fields' });
+                figure = this.createEls('figure', { className: 'portfolio-cover', title: project.name });
+                a      = this.createEls('a', { href: project.url });
+                img    = this.createEls('img', { className: 'portfolio-image', src: project.covers[404] });
+                h2     = this.createEls('h2', { className: 'portfolio-title' }, project.name);
 
                 for (i = 0, len = fields.length; i < len; i += 1) {
-                    fieldName = document.createTextNode(fields[i]);
-                    liF           = self.create('li', { className: 'field-item' });
+                    liF = this.createEls('li', { className: 'field-item' }, fields[i]);
 
-                    liF.appendChild(fieldName);
                     ul.appendChild(liF.cloneNode(true));
                 }
 
                 a.appendChild(img);
                 figure.appendChild(a);
                 div.appendChild(figure);
-                h2.appendChild(projectName);
                 div.appendChild(h2);
                 divF.appendChild(ul);
                 div.appendChild(divF);
                 li.appendChild(div);
 
                 elements.appendChild(li);
-                portfolio.appendChild(elements.cloneNode(true));
-            });
-        },
-        createHeader: function () {
-            var self     = this,
-                elements = document.createDocumentFragment(),
-                header;
+                portfolio.appendChild(elements);
+            }.bind(this));
 
-            header = self.create('header', { className: 'portfolio-header clearfix', id: 'header' });
-
-            elements.appendChild(header);
-            self.body.appendChild(elements);
-        },
-        createContent: function () {
-            var self     = this,
-                elements = document.createDocumentFragment(),
-                content, portfolio, portfolioList;
-
-            content       = self.create('div', { className: 'content-area clearfix', id: 'content' });
-            portfolio     = self.create('div', { className: 'portfolio-area clearfix', id: 'portfolio' });
-            portfolioList = self.create('ul', { className: 'portfolio-list clearfix' });
-
-            portfolio.appendChild(portfolioList);
-            content.appendChild(portfolio);
-
-            elements.appendChild(content);
-            self.body.appendChild(elements);
-        },
-        createFooter: function () {
-            var self        = this,
-                elements    = document.createDocumentFragment(),
-                poweredText = document.createTextNode('Powered by'),
-                be          = document.createTextNode('Behance'),
-                footer, power, powered, p, a;
-
-            footer  = self.create('footer', { className: 'portfolio-footer clearfix', id: 'footer' });
-            power   = self.create('div', { className: 'power-by', id: 'power' });
-            powered = self.create('p');
-            a       = self.create('a', { className: 'power-logo fi-social-behance', href: 'http://www.behance.net/', title: 'Behance' });
-            p       = self.create('p');;
-
-            a.appendChild(be);
-            p.appendChild(a);
-            powered.appendChild(poweredText);
-            power.appendChild(powered);
-            power.appendChild(p);
-            footer.appendChild(power);
-            
-            elements.appendChild(footer);
-            self.body.appendChild(elements);
-        },
-        loading: function (node) {
-            var self = this;
-            
-            node.removeChild(self.loader);
-        },
-        handleError: function () {
-            var self     = this,
-                elements = document.createDocumentFragment(),
-                p        = self.create('p', { className: 'error' }),
-                text     = document.createTextNode('An error ocurred, try again later.');
-
-            p.appendChild(text);
-            elements.appendChild(p);
-
-            self.body.appendChild(elements);
-
+            return this;
         },
         attach: function (data) {
-            var self = this;
+            var status = data && data.http_code;
 
-            self.createHeader();
-            self.createContent();
-            self.createFooter();
-            self.userProfile(data);
-            self.userProjects(data);
+            if (status === 200) {
+                this.body.removeChild(this.loader);
+                this.structureHeader().structureContent().structureFooter();
+                this.mountProfile(data).mountProjects(data);
+            } else {
+                this.fail();
+            }
+        },
+        fail: function () {
+            var elements = document.createDocumentFragment(),
+                p        = this.createEls('p', { className: 'error' }, 'An error ocurred, try again later.');
+
+            elements.appendChild(p);
+
+            this.body.removeChild(this.loader);
+            this.body.appendChild(elements);
         },
         request: function () {
-            var self = this,
-                body = self.body,
-                status;
-
-            if (!self.supportsSvg()) {
-                self.loader.src = './assets/css/img/loading.gif';
+            if (!this.supportsSvg()) {
+                this.loader.src = './public/css/img/loading.gif';
             }
 
-            self.getJSON({url: self.url}, function (data) {
-                status = data.http_code;
-
-                if (status === 200) {
-                    self.loading(body);
-                    self.attach(data);
-                } else {
-                    self.loading(body);
-                    self.handleError();
-                }
-            });
-        },
-        profile: function (config) {
-            module.config(config);
-            module.request();
+            this.getJSON(this.endpoint, this.attach.bind(this), this.fail.bind(this));
         }
     };
-    return {
-        profile: module.profile
-    };
-}(window, document));
+
+    return Behance;
+}));
